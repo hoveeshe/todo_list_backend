ab2684185a0e7eb87d4e97d842be8ade0966d6d0
hovees he <hovees.he@oocl.com>
2020-08-05 17:32:15 +0800

    [react-todolist-backend][hovees] feature: add TodoController

---
+++ src/main/java/com/thoughtworks/todolist/controller/TodoController.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.todolist.controller;
+
+import com.thoughtworks.todolist.entity.Todo;
+import com.thoughtworks.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    public List<Todo> findAll() {
+        return this.todoService.findAll();
+    }
+
+    @PostMapping
+    public Todo add(@RequestBody Todo todo) {
+        return this.todoService.add(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateById(@PathVariable Integer id) {
+        return this.todoService.updateById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteById(@PathVariable Integer id) {
+        return this.todoService.deleteById(id);
+    }
+
+}
