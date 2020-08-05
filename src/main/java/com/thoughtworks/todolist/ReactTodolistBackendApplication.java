078e96408721423a0b2c55970a82f448ea90ce96
hovees he <hovees.he@oocl.com>
2020-08-05 17:08:42 +0800

    [react-todolist-backend][hovees] feature: add Todo entity

---
+++ src/main/java/com/thoughtworks/todolist/ReactTodolistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTodolistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactTodolistBackendApplication.class, args);
+    }
+
+}
