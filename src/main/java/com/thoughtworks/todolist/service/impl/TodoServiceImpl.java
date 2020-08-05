8f0795f92746134a45365032bed3aee6a18dba79
hovees he <hovees.he@oocl.com>
2020-08-05 17:36:19 +0800

    [react-todolist-backend][hovees] feature: add TodoServiceImpl

---
+++ src/main/java/com/thoughtworks/todolist/service/impl/TodoServiceImpl.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.todolist.service.impl;
+
+import com.thoughtworks.todolist.dao.TodoDao;
+import com.thoughtworks.todolist.entity.Todo;
+import com.thoughtworks.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoServiceImpl implements TodoService {
+
+    @Autowired
+    private TodoDao todoDao;
+
+    @Override
+    public List<Todo> findAll() {
+        return this.todoDao.findAll();
+    }
+
+    @Override
+    public Todo add(Todo todo) {
+        return this.todoDao.save(todo);
+    }
+
+    @Override
+    public Todo updateById(Integer id) {
+        Todo todo = this.todoDao.getOne(id);
+        todo.setStatus(!todo.getStatus());
+        return this.todoDao.save(todo);
+    }
+
+    @Override
+    public String deleteById(Integer id) {
+        this.todoDao.deleteById(id);
+        return "delete success";
+    }
+}
