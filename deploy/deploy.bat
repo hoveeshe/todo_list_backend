48d44ff167bfac6de802a995e16e1c903b844ffa
hovees he <hovees.he@oocl.com>
2020-08-07 01:40:27 +0800

    [react-todolist][hovees] config: update Jenkinsfile

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -1,10 +1,10 @@
 API_NAME="todo-list-backend"
 API_VERSION="0.0.1"
 API_PORT="8010"
-IMAGE_NAME="$API_NAME:$BUILD_NUMBER"
-CONTAINER_NAME=$API_NAME-$API_VERSION
+IMAGE_NAME="${API_NAME}:${BUILD_NUMBER}"
+CONTAINER_NAME=${API_NAME}-${API_VERSION}
 
 copy ../build/libs/*.jar ./
-docker build -t $IMAGE_NAME .
+docker build -t ${IMAGE_NAME} .
 
-docker run --name $CONTAINER_NAME -d -p $API_PORT:8080 $IMAGE_NAME
+docker run --name ${CONTAINER_NAME} -d -p ${API_PORT}:8080 ${IMAGE_NAME}
