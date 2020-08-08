27f0863983e25beacf3de195831379ce08f7f4c9
hovees he <hovees.he@oocl.com>
2020-08-08 12:51:11 +0800

    test

--- deploy/deploy_dev.sh
+++ deploy/deploy_dev.sh
@@ -4,9 +4,20 @@ API_VERSION="0.0.1"
 API_PORT="8010"
 IMAGE_NAME=${API_NAME}:${API_VERSION}
 CONTAINER_NAME=${API_NAME}-${API_VERSION}
+DOCKER_FILE="./Dockerfile"
 
 docker rm -f ${CONTAINER_NAME}
 
+echo "**Init dockerfile start: "${DOCKER_FILE}
+echo "FROM tomcat" > ${DOCKER_FILE}
+echo 'MAINTAINER hovees "hovees.cn"' >> ${DOCKER_FILE}
+echo "ADD *.jar app.jar" >> ${DOCKER_FILE}
+echo "EXPOSE 8080" >> ${DOCKER_FILE}
+echo 'ENTRYPOINT ["java","-jar","app.jar"]' >> ${DOCKER_FILE}
+cat ${DOCKER_FILE}
+echo "**Init dockerfile end."
+
+echo "##Build dockerfile for "${IMAGE_NAME}
 cp ../build/libs/*.jar ./
 docker build -t ${IMAGE_NAME} .
 
